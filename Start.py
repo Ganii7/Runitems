19fdd500c7db0ff9aebb98c265e93abcb72c8c52
Ganii7 <84011962+Ganii7@users.noreply.github.com>
2024-02-23 21:12:59 +0100

    feat: read and write works
    Co-authored-by: David <M1n-74316D65@users.noreply.github.com>

--- Start.py
+++ Start.py
@@ -1,13 +1,18 @@
 import json
+
 jason = open('Aatrox.json')
 build = open('build.json')
 
-
 loadedbuild = json.load(build)
-#print(loadedbuild)
-
-
 loadedjason = json.load(jason)
-    #print(loadedjason)
+
 for i in loadedjason[0]["itemBuilds"][0]["blocks"]:
-    print(i)
+    for j in i["items"]:
+        # print(j)
+        loadedbuild["blocks"][0]["items"].append(j)
+
+
+
+print(loadedbuild)
+with open('build-1.json', 'w') as build:
+    json.dump(loadedbuild, build, indent=4)
