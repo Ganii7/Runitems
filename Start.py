521d529c72890f50e2963ea3b12a64a7f2e65d76
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-02-26 17:54:50 +0100

    refactor: :recycle: clean up and refactor
    Much, much cleaner and faster.

--- Start.py
+++ Start.py
@@ -1,38 +1,21 @@
 import json
 
-jason = open('Aatrox.json')
-build = open('build.json')
+objetosfinales = set()
+loadedbuild = {}
 
-loadedbuild = json.load(build)
-loadedjason = json.load(jason)
+with open('Aatrox.json') as jason, open('build.json') as build:
+    loadedbuild = json.load(build)
+    loadedjason = json.load(jason)
 
-numi = 0
-numfinal = 0
-objetosfinales = []
+    for i, items in enumerate(loadedjason[0]["itemBuilds"][0]["blocks"]):
+        for item in items["items"]:
+            numfinal = 1 if i <= 2 or i <= 4 else 2
 
-for items in loadedjason[0]["itemBuilds"][0]["blocks"]:
-    numj = 0
+            if i == 2 or item["id"] not in objetosfinales:
+                loadedbuild["blocks"][numfinal]["items"].append(item)
+            objetosfinales.add(item["id"])
 
-    for item in items["items"]:
-        # print(j)
+# print(loadedbuild)
 
-        if numi <= 2:
-            if numi == 2:
-                # This line is duplicated because the third object has to be duplicated
-                loadedbuild["blocks"][0]["items"].append(item)
-                numfinal = 1
-        elif numj == 0 or numi <= 4:
-            numfinal = 1
-        else:
-            numfinal = 2
-
-        if item["id"] not in objetosfinales:
-            loadedbuild["blocks"][numfinal]["items"].append(item)
-        objetosfinales.append(item["id"])
-        numi += 1
-        numj += 1
-
-
-print(loadedbuild)
 with open('build-1.json', 'w') as build:
     json.dump(loadedbuild, build, indent=4)
