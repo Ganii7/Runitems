a6f5a6c83bec633e0386a4f20c73c61b6a0b4eec
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-02-26 21:08:05 +0100

    refactor: :recycle: complete refactoring and improvement of the sorting process

--- Start.py
+++ Start.py
@@ -1,21 +1,48 @@
 import json
 
-objetosfinales = set()
-loadedbuild = {}
+def add_item(item, final_objects, loaded_build, block_index):
+    # If the item is not in final objects
+    if item["id"] not in final_objects:
+        # Add the item to the loaded build
+        loaded_build["blocks"][block_index]["items"].append(item)
+        # Add the item's id to the final objects
+        final_objects.add(item["id"])
+    # Return the updated final objects and loaded build
+    return final_objects, loaded_build
 
-with open('Aatrox.json') as jason, open('build.json') as build:
-    loadedbuild = json.load(build)
-    loadedjason = json.load(jason)
+def process_json_files(aatrox_file, build_file):
+    with open(aatrox_file) as aatrox, open(build_file) as build:
+        loaded_build = json.load(build)
+        loaded_aatrox = json.load(aatrox)
+        final_objects = set()
+        alternative_items = []
 
-    for i, items in enumerate(loadedjason[0]["itemBuilds"][0]["blocks"]):
-        for item in items["items"]:
-            numfinal = 1 if i <= 2 or i <= 4 else 2
+        # Clear the items lists in the blocks of the loaded build
+        for block in loaded_build["blocks"]:
+            block["items"].clear()
 
-            if i == 2 or item["id"] not in objetosfinales:
-                loadedbuild["blocks"][numfinal]["items"].append(item)
-            objetosfinales.add(item["id"])
+        for i, items in enumerate(loaded_aatrox[0]["itemBuilds"][0]["blocks"]):
+            for x, item in enumerate(items["items"]):
+                # Determine which block to add the item to
+                if i == 1 or (i >= 1 and x == 0 and (len(loaded_build["blocks"][1]["items"]) < 6)):
+                    final_objects, loaded_build = add_item(item, final_objects, loaded_build, 1)
+                    # Boots exception
+                    if x == 0 and i == 1:
+                        loaded_build["blocks"][0]["items"].append(item)
+                elif i == 0:
+                    final_objects, loaded_build = add_item(item, final_objects, loaded_build, 0)
+                else:
+                    alternative_items.append(item)
 
-# print(loadedbuild)
+        for alternative_item in alternative_items:
+            final_objects, loaded_build = add_item(alternative_item, final_objects, loaded_build, 2)
 
-with open('build-1.json', 'w') as build:
-    json.dump(loadedbuild, build, indent=4)
+    return loaded_build
+
+def write_to_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
+
+if __name__ == "__main__":
+    modified_build = process_json_files('Aatrox.json', 'build.json')
+    write_to_file(modified_build, 'build-1.json')
