b1142421eadd1d57b8b0e3ad9193257920eb04d1
Ganii7 <84011962+Ganii7@users.noreply.github.com>
2024-02-26 17:44:46 +0100

    feat: order objects
    Co-authored-by: David <M1n-74316D65@users.noreply.github.com>

--- Start.py
+++ Start.py
@@ -6,10 +6,50 @@ build = open('build.json')
 loadedbuild = json.load(build)
 loadedjason = json.load(jason)
 
-for i in loadedjason[0]["itemBuilds"][0]["blocks"]:
-    for j in i["items"]:
+numi = 0
+numfinal = 0
+objetosfinales = []
+
+for items in loadedjason[0]["itemBuilds"][0]["blocks"]:
+    numj = 0
+    
+    for item in items["items"]:
         # print(j)
-        loadedbuild["blocks"][0]["items"].append(j)
+        
+        
+        if numi <= 2:
+            if numi == 2:
+                # This line is duplicated because the third object has to be duplicated
+                loadedbuild["blocks"][0]["items"].append(item)
+                numfinal = 1
+        elif numj == 0 or numi <=4:
+            numfinal = 1
+        else:
+            numfinal = 2
+
+        if item["id"] not in objetosfinales:
+            loadedbuild["blocks"][numfinal]["items"].append(item)
+        objetosfinales.append(item["id"])
+        numi += 1
+        numj += 1
+
+
+
+# Hacer array guardando todas las id de los items core para que no se repitan
+# ni en core ni en alternativos
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
