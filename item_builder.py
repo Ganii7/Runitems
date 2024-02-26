a14f81ad6546a1151aff2f27f38aaefa44e16a87
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-02-27 00:33:09 +0100

    style: :art: more autoformatting

--- item_builder.py
+++ item_builder.py
@@ -20,30 +20,35 @@ def process_json_files(aatrox_file, build_file):
 
         for i, items in enumerate(loaded_aatrox[0]["itemBuilds"][0]["blocks"]):
             for x, item in enumerate(items["items"]):
-                if i == 1 or (i >= 1 and x == 0 and (len(loaded_build["blocks"][1]["items"]) < 6)):
+                if i == 1 or (
+                    i >= 1 and x == 0 and (len(loaded_build["blocks"][1]["items"]) < 6)
+                ):
                     final_objects, loaded_build = add_item(
-                        item, final_objects, loaded_build, 1)
+                        item, final_objects, loaded_build, 1
+                    )
                     # Boots exception
                     if x == 0 and i == 1:
                         loaded_build["blocks"][0]["items"].append(item)
                 elif i == 0:
                     final_objects, loaded_build = add_item(
-                        item, final_objects, loaded_build, 0)
+                        item, final_objects, loaded_build, 0
+                    )
                 else:
                     alternative_items.append(item)
 
         for alternative_item in alternative_items:
             final_objects, loaded_build = add_item(
-                alternative_item, final_objects, loaded_build, 2)
+                alternative_item, final_objects, loaded_build, 2
+            )
 
     return loaded_build
 
 
 def write_to_file(data, filename):
-    with open(filename, 'w') as file:
+    with open(filename, "w") as file:
         json.dump(data, file, indent=4)
 
 
 if __name__ == "__main__":
-    modified_build = process_json_files('Aatrox.json', 'build.json')
-    write_to_file(modified_build, 'build-1.json')
+    modified_build = process_json_files("Aatrox.json", "build.json")
+    write_to_file(modified_build, "build-1.json")
