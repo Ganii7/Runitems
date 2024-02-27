4f50bce4916cb8c25467b1aba10f58345c18b261
Ganii7 <84011962+Ganii7@users.noreply.github.com>
2024-02-27 18:47:35 +0100

    feat: refactor order item
    Agreement to refactor using the new logic to sorte items, Co-authored-by: David <M1n-74316D65@users.noreply.github.com>

--- Start.py
+++ Start.py
@@ -1,38 +1,54 @@
 import json
 
-jason = open('Aatrox.json')
-build = open('build.json')
 
-loadedbuild = json.load(build)
-loadedjason = json.load(jason)
+def read_build_json(champion):
+    jason = open(champion)
+    build = open('build.json')
+    loadedbuild = json.load(build)
+    loadedjason = json.load(jason)
+    objetosfinales = []
+    objetosrepetidos = []
+    switch = True
 
-numi = 0
-numfinal = 0
-objetosfinales = []
+    def write_item(item,numfinal):
+        loadedbuild["blocks"][numfinal]["items"].append(item)
+    
+    
+    
+    for i,items in enumerate(loadedjason[0]["itemBuilds"][0]["blocks"]):
+        for j,item in enumerate(items["items"]):
+            # print(j)
 
-for items in loadedjason[0]["itemBuilds"][0]["blocks"]:
-    numj = 0
-
-    for item in items["items"]:
-        # print(j)
-
-        if numi <= 2:
-            if numi == 2:
-                # This line is duplicated because the third object has to be duplicated
-                loadedbuild["blocks"][0]["items"].append(item)
+            if i == 0:
+                numfinal = 0
+            elif i ==1 and j ==0:
+                write_item(item,0)
+                numfinal= 1
+            elif i == 1:
                 numfinal = 1
-        elif numj == 0 or numi <= 4:
-            numfinal = 1
-        else:
-            numfinal = 2
+                objetosrepetidos.append(item["id"])
+            elif i> 1 and item["id"] not in objetosrepetidos:
+                if j == 0:
+                    numfinal = 1
+                    objetosrepetidos.append(item["id"])
+                    switch = True
+                elif item["id"] not in objetosrepetidos:
+                    objetosfinales.append(item)
+                    objetosrepetidos.append(item["id"])
+                    switch = False
+            if switch:
+                write_item(item,numfinal)
+    while len(loadedbuild["blocks"][1]["items"]) <= 5:
+        write_item(objetosfinales[0],1)
+        del objetosfinales[0]
+    write_item(objetosfinales,2)
+                
+                
+    return loadedbuild
 
-        if item["id"] not in objetosfinales:
-            loadedbuild["blocks"][numfinal]["items"].append(item)
-        objetosfinales.append(item["id"])
-        numi += 1
-        numj += 1
-
-
-print(loadedbuild)
-with open('build-1.json', 'w') as build:
-    json.dump(loadedbuild, build, indent=4)
+def write_to_file(archivo,contenido):
+    with open(archivo, 'w') as build:
+        json.dump(contenido, build, indent=4)
+    
+if __name__ == "__main__":
+    write_to_file("build-1.json",read_build_json('Aatrox.json'))
