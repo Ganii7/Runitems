6ce74f466faa062629dee07dc0c90bb567facdb0
Ganii7 <84011962+Ganii7@users.noreply.github.com>
2024-03-07 19:23:07 +0100

    feat: rebuild by just letting the import system
    Co-authored-by: David <M1n-74316D65@users.noreply.github.com>

--- file_updater.py
+++ file_updater.py
@@ -1,5 +1,6 @@
 import git
 import json
+import os
 
 
 def write_json(data, filepath):
@@ -7,7 +8,7 @@ def write_json(data, filepath):
         json.dump(data, file, indent=4)
 
 
-def update_lol_file(data, lol_filepath):
+def update_lol_file(filename, lol_filepath):
     """
     Update the League of Legends (LoL) file with new data.
 
@@ -20,9 +21,10 @@ def update_lol_file(data, lol_filepath):
     """
 
     lol_filepath += "\Config\ItemSets.json"
-    with open(lol_filepath) as old_lol_file:
+    
+    with open(lol_filepath) as old_lol_file, open(f"champions/{filename}") as data:
         lol_data = json.load(old_lol_file)
-        lol_data["itemSets"].append(data)
+        lol_data["itemSets"].append(json.load(data))
     write_json(lol_data, lol_filepath)
 
 
@@ -42,5 +44,9 @@ def update_local_files(folder):
 
 
 if __name__ == "__main__":
-    file = r"../champions"
+    file = r"champions"
     update_local_files(file)
+    for filename in os.listdir("./champions"):
+        if filename.endswith(".json"):
+            update_lol_file(filename, r"C:\Games\Riot Games\League of Legends")
+            print(filename)
