f98a3f547b59a4f8d96e1ad147074b078903f281
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-03-09 23:21:21 +0100

    feat(UI): :lipstick: first GTK screen
    We have to test it on windows

--- file_updater.py
+++ file_updater.py
@@ -10,43 +10,63 @@ def write_json(data, filepath):
 
 def update_lol_file(filename, lol_filepath):
     """
-    Update the League of Legends (LoL) file with new data.
+    Updates a League of Legends file with new item sets.
 
-    Args:
-        data: The new data to be added to the LoL file.
-        lol_filepath: The file path to the LoL file.
+    Parameters:
+    - filename: a string representing the name of the file to be updated
+    - lol_filepath: a string representing the path to the League of Legends file
 
-    Returns:
-        None
+    Return:
+    This function does not return anything.
     """
-
     lol_filepath += "\Config\ItemSets.json"
-    
+
     with open(lol_filepath) as old_lol_file, open(f"champions/{filename}") as data:
         lol_data = json.load(old_lol_file)
         lol_data["itemSets"].append(json.load(data))
     write_json(lol_data, lol_filepath)
 
 
-def update_local_files(folder):
+def update_local_files(switch):
     """
-    Update local files by pulling the specified repository. If the repository does not exist, clone it and print an error message.
+    Updates local files by pulling changes from the remote git repository.
 
-    :param folder: the folder to update
-    :return: None
+    Args:
+        switch (bool): A boolean flag indicating whether to create all lanes git repo.
+
+    Returns:
+        None
     """
     try:
-        git.Repo(folder).remotes.origin.pull()
+        git.Repo(r"champions").remotes.origin.pull()
     except git.exc.NoSuchPathError:
-        git.Repo.clone_from(
-            'https://github.com/Ganii7/Runeitems-champions.git', folder)
+        if switch:
+            # TODO: create all lanes git repo
+            # TODO: add all lanes git
+            print("pls create all lanes git")
+        else:
+            git.Repo.clone_from(
+                'https://github.com/Ganii7/Runeitems-champions.git', r"champions")
         print("Error to pull, cloning repository")
 
 
+def main(switch):
+    if switch:
+        # TODO: import all lanes
+        print("import all lanes")
+        # update_local_files(switch)
+        # for filename in os.listdir("./champions"):
+        #     if filename.endswith(".json"):
+        #         update_lol_file(filename, r"C:\Riot Games\League of Legends")
+        #         print(filename)
+    else:
+        print("import most popular lane")
+        # update_local_files(switch)
+        # for filename in os.listdir("./champions"):
+        #     if filename.endswith(".json"):
+        #         update_lol_file(filename, r"C:\Riot Games\League of Legends")
+        #         print(filename)
+
+
 if __name__ == "__main__":
-    file = r"champions"
-    update_local_files(file)
-    for filename in os.listdir("./champions"):
-        if filename.endswith(".json"):
-            update_lol_file(filename, r"C:\Riot Games\League of Legends")
-            print(filename)
+    main(False)
