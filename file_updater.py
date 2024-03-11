a2916e405ba279821aa500ccb12fe632afe131ff
M1n <54779580+M1n-74316D65@users.noreply.github.com>
2024-03-11 17:46:49 +0100

    Revert "test: Testing for champion builds"
    This reverts commit 03fdb78ee12fc0a9fa26ccf1b827c4dd273212d2.

--- file_updater.py
+++ file_updater.py
@@ -1,15 +1,11 @@
 import git
 import json
 import os
-import shutil
 
 
 def write_json(data, filepath):
     with open(filepath, "w") as file:
         json.dump(data, file, indent=4)
-        
-def copie_file(data, filepath):
-    shutil.copyfile(data,filepath)
 
 
 def update_lol_file(filename, lol_filepath):
@@ -50,18 +46,7 @@ def update_local_files(folder):
 if __name__ == "__main__":
     file = r"champions"
     update_local_files(file)
-    champion = input("Enter champion name:")
     for filename in os.listdir("./champions"):
-        if filename.endswith(".json") and filename.startswith(champion):
-            update_lol_file(filename, r"C:\Games\Riot Games\League of Legends")
-            # copie_file(f"./champions/{filename}" , r"C:\Games\Riot Games\League of Legends\Config\Champions/"+str(filename.removesuffix(".json"))+r"\Recommended/"+filename)
+        if filename.endswith(".json"):
+            update_lol_file(filename, r"C:\Riot Games\League of Legends")
             print(filename)
-    
-    # for filename in os.listdir("./champions"):
-    #     if filename.endswith(".json"):
-    #         update_lol_file(filename, r"C:\Games\Riot Games\League of Legends")
-    #         print(filename)
-    
-    
-
-    
